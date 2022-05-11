f46fe1b0f3abea9144283939f13c16e013f50ff3
Pickacho <40912416+Pickacho@users.noreply.github.com>
2022-05-11 12:44:54 +0300

    Add files via upload

--- Terrafrom/main.tf
+++ Terrafrom/main.tf
@@ -3,7 +3,11 @@ resource "local_file" "pet" {
     content = var.content
 }
 resource "random_pet" "my-pet" {
-    prefix = var.prefix
+    prefix = var.prefix[2]
     separator = var.separator
     length = var.length
+}
+resource "local_file" "my-pet" {
+    filename = "/home/thor/Documents/GitHub/Ansible_playbook_examples/Terrafrom/terraform-local-file/statement.txt"
+    content = var.file-content["statement2"] 
 }
--- Terrafrom/variables.tf
+++ Terrafrom/variables.tf
@@ -2,14 +2,34 @@ variable "filename" {
     default = "/home/thor/Documents/GitHub/Ansible_playbook_examples/Terrafrom/terraform-local-file/pets.txt"
 }
 variable "content" {
-    default = "We love pets!"
+    default = "My favorite pet is Mrs. Whiskers"
 }
-variable "prefix" {
+/* variable "prefix" {
     default = "Mrs"
-}
+} */
 variable "separator" {
     default = "."
 }
 variable "length" {
-    default = "1"
+    default = "2"
+}
+variable "prefix" {
+    default = ["Mrs", "Mr", "Sir"]
+    type = list
+}
+
+variable file-content {
+type = map
+default = {
+    "statement1" = "We love pets!"
+    "statement2" = "We love animals"
+    }
+}
+variable "prefixsirstringname" {
+    default = ["Mrs", "Mr", "Sir"]
+    type =  list(string)
+}
+variable "prefixsirnamenumber" {
+    default = [ 1 , 2, 3]
+    type =  list(number)
 }
